39dd48969e81fecbe58052df0680d40f3037e212
VilmaTuresson <vilma.turesson@gmail.com>
2022-03-30 16:19:33 +0000

    create post model with meta class to order posts

--- blogg/models.py
+++ blogg/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.Charfield(max_length=200)
+    post_id = models.BigAutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post_img = CloudinaryField('image')
+    likes = models.ManyToManyField(User, related_name='blog_likes')
+
+    class Meta:
+        ordering = ['-created_on']
+
