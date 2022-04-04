6f7a2c7b3f8d7e3e0b86bad74a8205441b383d65
VilmaTuresson <vilma.turesson@gmail.com>
2022-04-04 18:05:37 +0000

    add view for base.html template

--- blogg/views.py
+++ blogg/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
+class PostView(generic.ListView):
+    """
+    View for home page with list of posts
+    """
+    model = Post
+    queryset = Post.objects.order_by('-created_on')
+    template_name = 'base.html'
+    paginated_by = 20
