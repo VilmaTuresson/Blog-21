99a9f0381f32447c7c5ac902129392dec275d8d9
VilmaTuresson <vilma.turesson@gmail.com>
2022-04-05 12:02:29 +0000

    create url path for post details and add link on post title to open post

--- blogg/urls.py
+++ blogg/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name='home')
+    path('', views.PostView.as_view(), name='home'),
+    path('article/<int:pk>', views.PostDetails.as_view(), name='post_details')
 ]
