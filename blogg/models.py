0e667cc5cac4407744c4e5c955ed738a1ead7ddc
VilmaTuresson <vilma.turesson@gmail.com>
2022-04-05 11:18:32 +0000

    fixed post-model so img is optional for post

--- blogg/models.py
+++ blogg/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    post_img = CloudinaryField('image')
+    post_img = CloudinaryField('image', blank=True)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
