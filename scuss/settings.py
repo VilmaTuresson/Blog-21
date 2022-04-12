9e2cc6e46a9cbbadc77726cde23c9f490c9901e6
VilmaTuresson <vilma.turesson@gmail.com>
2022-04-12 15:57:59 +0000

    create user authentication

--- scuss/settings.py
+++ scuss/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'blogg',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'post_list_view'
+LOGOUT_REDIRECT_URL = 'post_list_view'
