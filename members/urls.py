4c74255712a3a6a85f8294282a8fca08eec88da5
VilmaTuresson <vilma.turesson@gmail.com>
2022-04-14 14:32:04 +0000

    create view, path and form for  user to edit profile

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegistration
+from .views import UserRegistration, UserEditProfile
 
 urlpatterns = [
     path('register/', UserRegistration.as_view(), name='register'),
+    path('edit_profile/', UserEditProfile.as_view(), name='edit_profile'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 
 
@@ -11,3 +11,18 @@ class UserRegistration(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('post_list_view')
+
+
+class UserEditProfile(generic.UpdateView):
+    """
+    View for updating profile
+    """
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('post_list_view')
+
+    def get_object(self):
+        """
+        function to return users profile
+        """
+        return self.request.user
